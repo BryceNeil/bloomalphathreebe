927516c4f9fb0407035017965c6a36f901d16ec9
Bryce Neil <100434551+BryceNeil@users.noreply.github.com>
2024-04-30 21:03:42 -0400

    Update constants.py

--- app/misc/constants.py
+++ app/misc/constants.py
@@ -1,15 +1,15 @@
 class _Secrets:
     ENV_NAME = "local"
-    OPENAI_KEY = "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"
-    GOOGLE_SEARCH_API_KEY = 'AIzaSyCM25-G78V03FwcHA-OspNjOpej_qgwTE8'
-    GOOGLE_SEARCH_CSE_ID = 'c5548d48e52cc4107'
+    OPENAI_KEY = ""
+    GOOGLE_SEARCH_API_KEY = ''
+    GOOGLE_SEARCH_CSE_ID = ''
 
-    GROQ_API_KEY = "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"
+    GROQ_API_KEY = ""
 
-    ELEVEN_LABS_API_KEY = "747d3d9c9e3b0e9ea6b3336183e7e8a1"
+    ELEVEN_LABS_API_KEY = ""
 
     # Google Custom Search JSON API endpoint
-    GOOGLE_SEARCH_API_ENDPOINT = 'https://www.googleapis.com/customsearch/v1'
+    GOOGLE_SEARCH_API_ENDPOINT = ''
     
 
 GPT_MODEL = "gpt-3.5-turbo-1106"
